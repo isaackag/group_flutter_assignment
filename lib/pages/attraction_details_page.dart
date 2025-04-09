5741d899783de8fd48b6c75fef190351dc958151
Isaac Kagwanja <ithuo10@gmail.com>
2025-04-09 12:09:39 +0300

    added first 2 page files

---
+++ lib/pages/attraction_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AttractionDetailsPage extends StatelessWidget {
+  const AttractionDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
