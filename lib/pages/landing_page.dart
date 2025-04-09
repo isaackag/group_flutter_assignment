47d855145275ed5d0ee70eca74e9fde12e80f0d9
Twizzy-dev001 <allanmwangi707@gmail.com>
2025-04-09 12:26:23 +0300

    changed landing page and main

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:group_flutter_assignment/widgets/image_widget.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body:  ImageWidget(),
+      
+    );
+  }
+}
