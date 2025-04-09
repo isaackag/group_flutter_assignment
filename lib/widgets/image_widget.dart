b14038f60597463302a40d86be8ef85660565300
Twizzy-dev001 <allanmwangi707@gmail.com>
2025-04-09 11:46:23 +0300

    changed photo

---
+++ lib/widgets/image_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ImageWidget extends StatelessWidget {
+  const ImageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(50),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade300,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          color: Colors.grey.shade50,
+          image:  DecorationImage(
+            image: AssetImage('assets/images/img1.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
